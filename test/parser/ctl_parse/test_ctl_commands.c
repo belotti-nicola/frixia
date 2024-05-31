4f0d3519512708bd815648ef9a53d53929cb56fd
belotti-nicola <belotti.nicola@outlook.it>
2024-05-31 16:52:42 +0200

    current state of the branc implementing test

--- test/parser/ctl_parse/test_ctl_commands.c
+++ test/parser/ctl_parse/test_ctl_commands.c
@@ -1,92 +1,41 @@
-#include <dirent.h>
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <string.h>
 
 #include "../../../src/core/ctl_parser/control_strings_parser.h"
 #include "../../../src/core/ctl_parser/control_commands.h"
 
-
-#define TEST_DIR_ERRORS "test_strings/parse_errors"
-#define TEST_DIR_OKS "test_strings/parse_oks"
-
+#define data "test_strings/samples.csv"
 
 int main(void)
 {
-    DIR *d;
-    struct dirent *dir;
     struct FrixiaCTL fctl;
     FILE *fptr;
-    char path[200] = "";
-    char parse_this[50];
-    int parse_exit_code = -1;
+    char* line = NULL;
+    char* token = NULL;
+    size_t len = 0;
 
-    d = opendir(TEST_DIR_ERRORS);
-    if (d)
+    fptr = fopen(data,"r");
+    if(fptr == NULL)
     {
-        while ((dir = readdir(d)) != NULL)
-        {
-            printf("%s\n",dir->d_name);
-            memset(path,0,strlen(path));
-            strcat(path,TEST_DIR_ERRORS);
-            strcat(path,"/");
-            strcat(path,dir->d_name);
-            if( strcmp("..",path) == 0 ) {
-                printf("Skipping ..\n");
-                break;
-            }
-            printf("path %s\n",path);
-            fptr = fopen(path, "r");
-            if (fptr == NULL)
-            {
-                printf("TEST FAILED CANT OPEN %s\n",path);
-            }
-            if( fgets(parse_this,50,fptr) == NULL) {
-                printf("TEST FAILED FGETS %s\n",path);
-            }
-            parse_exit_code = parse_control_strings(parse_this,&fctl);
-            if(parse_exit_code == PARSE_OK){
-                printf("TEST FAILED %s EXPECTING PARSE_ERROR\n",path);
-                closedir(d);
-                return 1;
-            }
-            fclose(fptr);
-        }
-        closedir(d);
-    }
-
-    d = opendir(TEST_DIR_OKS);
-    if (d)
-    {
-        while ((dir = readdir(d)) != NULL)
-        {
-            printf("%s\n",dir->d_name);
-            memset(path,0,strlen(path));
-            strcat(path,TEST_DIR_ERRORS);
-            strcat(path,"/");
-            strcat(path,dir->d_name);
-            if( strcmp("..",path) == 0 ) {
-                printf("Skipping ..\n");
-                break;
-            }
-            printf("path %s\n",path);
-            fptr = fopen(path, "r");
-            if (fptr == NULL)
-            {
-                printf("TEST FAILED CANT OPEN %s\n",path);
-            }
-            if( fgets(parse_this,50,fptr) == NULL) {
-                printf("TEST FAILED FGETS %s\n",path);
-            }
-            parse_exit_code = parse_control_strings(parse_this,&fctl);
-            if(parse_exit_code == PARSE_OK){
-                printf("TEST FAILED %s EXPECTING PARSE_ERROR\n",path);
-                closedir(d);
-                return 1;
-            }
-            fclose(fptr);
-        }
-        closedir(d);
+        return 1;
     }
     
+    
+    char* dirname = NULL;
+    char* filename = NULL;
+    char* return_code = NULL;
+    char* type = NULL;
+    char* argument = NULL;
+    while( getline(&line,&len,fptr) != -1 )
+    {
+        dirname = strtok(line,",");
+        filename = strtok(line,",");
+        return_code = strtok(line,",");
+        type = strtok(line,",");
+        argument = strtok(line,",");
+    }
+
+    fclose(fptr);
     return 0;
 }
