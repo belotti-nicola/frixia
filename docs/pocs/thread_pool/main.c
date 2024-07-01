904d321766f82cee8d363e279d1a9d482b6e1c7c
belotti-nicola <belotti.nicola@outlook.it>
2024-07-01 19:43:40 +0200

    b4 switch

--- docs/pocs/thread_pool/main.c
+++ docs/pocs/thread_pool/main.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<pthread.h>
+#include<unistd.h>
 
-#define Q_LENGTH 40
+#define Q_ELEMENTS 40
+#define THREAD_POOL_DIMENSION 5
 
 static void foo(int i)
 {
+    int r = rand() % 5;
+    sleep(r);
     printf("%d\n",i);
 }
 
@@ -60,15 +65,33 @@ simple_queue_t* create_simple_list(size_t n)
 
 }
 
+typedef struct thread_pool
+{
+    size_t dim;
+
+} thread_pool_t;
+
+void create_thread_pool(int n)
+{
+    pthread_t* thread;
+    for (int i=0; i<n; i++) 
+    {
+        pthread_create( &thread, NULL, foo, );
+    }
+}
+
 
 int main()
 {
-    simple_queue_t* sq = create_simple_list(Q_LENGTH);
+    simple_queue_t* sq = create_simple_list(Q_ELEMENTS);
     simple_queue_el_t* sqel = sq->first;
-    for(int i=0;i<Q_LENGTH;i++)
+    for(int i=0;i<Q_ELEMENTS;i++)
     {
         printf("%d\n",sqel->arg);
         sqel = sqel->next;
     }
+
+    create_thread_pool(THREAD_POOL_DIMENSION);
+
     return 0;
 }
