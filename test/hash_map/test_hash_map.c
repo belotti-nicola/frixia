872bff248b26c893bed4422e6184dd2552548208
belotti-nicola <belotti.nicola@outlook.it>
2024-07-15 13:50:39 +0200

    implementing hash map

---
+++ test/hash_map/test_hash_map.c
@@ -0,0 +1,12 @@
+#include "../../src/core/callback_suite/hashmap/hash_map.h"
+
+int main()
+{
+    HashMap_t* hm = create_hash_map(5);
+    
+    //implement this.
+    //add_entry(hm,NULL);
+    
+    destroy_hash_map(hm);
+    return 0;
+}
