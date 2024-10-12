693257b09893958e793bfc3ccf388987cef28884
belotti-nicola <belotti.nicola@outlook.it>
2024-10-12 17:13:36 +0200

    refactoring epoll

---
+++ src/core/filedescriptor/fd_monitor/epoll/fepoll.c
@@ -0,0 +1,7 @@
+#include "fepoll.h"
+#include "fepoll_codes.h"
+
+FRIXIA_EPOLL_CODE_T start_epoll();
+FRIXIA_EPOLL_CODE_T stop_epoll();
+FRIXIA_EPOLL_CODE_T add_tcp_listener();
+FRIXIA_EPOLL_CODE_T stop_tcp_listener();
---
+++ src/core/filedescriptor/fd_monitor/epoll/fepoll.h
@@ -0,0 +1,6 @@
+#include "fepoll_codes.h"
+
+FRIXIA_EPOLL_CODE_T start_epoll();
+FRIXIA_EPOLL_CODE_T stop_epoll();
+FRIXIA_EPOLL_CODE_T add_tcp_listener();
+FRIXIA_EPOLL_CODE_T stop_tcp_listener();
