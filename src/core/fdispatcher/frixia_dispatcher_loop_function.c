6ed08b39657f0434ecdd7d5c2314c560c5b22357
belotti-nicola <belotti.nicola@outlook.it>
2024-12-04 17:42:51 +0100

    dispatcher loop function

--- src/core/fdispatcher/frixia_dispatcher_loop_function.c
+++ src/core/fdispatcher/frixia_dispatcher_loop_function.c
@@ -1,6 +1,20 @@
+#include <stdbool.h>
+#include "frixia_dispatcher.h"
+
 #include "frixia_dispatcher_loop_function.h"
 
-int frixia_dispatcher_loop_function()
+int frixia_dispatcher_loop_function(frixia_dispatcher_t *dispatcher)
 {
+    frixia_events_queue_t *events_queue = dispatcher->tasks;
+    frixia_event_t        *event;
+    
+    bool keep_looping = true;
+    while(true)
+    {
+        event = frixia_events_queue_pop(events_queue);
+        printf("EVENT POPPED BY DISPATCHER::%d\n",event->fd);
+    }
+
+    destroy_frixia_dispatcher(dispatcher);
     return 0;
 }
--- src/core/fdispatcher/frixia_dispatcher_loop_function.h
+++ src/core/fdispatcher/frixia_dispatcher_loop_function.h
@@ -1,1 +1,3 @@
-int frixia_dispatcher_loop_function();
+#include "frixia_dispatcher.h"
+
+int frixia_dispatcher_loop_function(frixia_dispatcher_t *dispatcher);
