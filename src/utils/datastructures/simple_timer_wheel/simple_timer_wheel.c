f471c8e1557fe1ee9846dffda786b6ec2efba92b
belotti-nicola <belotti.nicola@outlook.it>
2025-03-21 00:40:47 +0100

    adding periodic contexts

--- src/utils/datastructures/simple_timer_wheel/simple_timer_wheel.c
+++ src/utils/datastructures/simple_timer_wheel/simple_timer_wheel.c
@@ -3,6 +3,37 @@
 
 #include "simple_timer_wheel.h"
 
+typedef struct periodic_context
+{
+    simple_timer_wheel_t *tw;
+    int delay;
+    void (*fun)(void *);
+    void *arg;
+
+} periodic_context_t;
+
+void *periodic_timer_callback(periodic_context_t *ctx)
+{
+    if(ctx->fun == NULL )
+    {
+        return;
+    }
+    if(ctx->arg == NULL )
+    {
+        return;
+    }
+
+    ctx->fun(ctx->arg);
+    simple_timer_wheel_add_oneshot_timer(
+        ctx->tw,
+        ctx->delay,
+        ctx->fun,
+        ctx->arg
+    );
+
+}
+
+
 void simple_timer_wheel_add_oneshot_timer(simple_timer_wheel_t *tw, int delay,void (*fun)(void *), void *arg)
 {
     int slots = TIMER_WHEEL_SLOT_SIZE;
@@ -25,9 +56,18 @@ void simple_timer_wheel_add_periodic_timer(simple_timer_wheel_t *tw, int delay,v
     int target_round = delay / wheel_duration;
 
     simple_wheel_slot_t *slot = tw->slots;
-    simple_wheel_slot_add_timer(slots+target_slot,fun,arg);   
+    periodic_context_t ctx = {
+        .tw = tw,
+        .delay = delay,
+        .fun = fun,
+        .arg = arg
+    };
+    simple_wheel_slot_add_timer(slots+target_slot,periodic_timer_callback,&ctx); 
 }
 void simple_timer_wheel_tick(simple_timer_wheel_t *tw)
 {
-
+    int index = tw->current_index++;
+    simple_wheel_slot_t *slot = tw->slots+index;
+    simple_wheel_slot_remove_timers(slot);
+    
 }
