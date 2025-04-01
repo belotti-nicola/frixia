baa5722285d0a4c0b8fb5d7819cb6445d1c73364
belotti-nicola <belotti.nicola@outlook.it>
2025-04-02 00:16:46 +0200

    current state of the branch

--- src/core/thread_pool/bound_robin/bound_robin.c
+++ src/core/thread_pool/bound_robin/bound_robin.c
@@ -2,18 +2,35 @@
 
 int bound_robin_add_task(bound_robin_t *br, void *task)
 {
-    int current_index = br->current_index;
-
-    threadsafe_simple_queue_t *target = br->event_in[current_index];
-    push_threadsafe_simple_queue(target,task);
-
-    br->current_index = (current_index + 1)%FRIXIA_WORKERS;
+    return;
 }
 int bound_robin_broadcast_task(bound_robin_t *br, void *task)
 {
-    for( int i=0;i<FRIXIA_WORKERS;i++)
+    return;
+}
+
+void delegate_target_thread_with_policy(bound_robin_t *br,int index,void *task)
+{
+
+}
+
+bound_robin_t bound_robin_create(void *th_fun(void *),void *th_arg,
+                                 void *th_delegate_fun(void *),void *th_delegate_arg)
+{
+    bound_robin_t br;
+    pthread_t *thread;
+
+    br.delegate_function = th_delegate_fun;
+    br.delegate_argument = th_delegate_arg;
+
+    for(int i=0;i<FRIXIA_WORKERS;i++)
     {
-        threadsafe_simple_queue_t *target = br->event_in[i];
-        push_threadsafe_simple_queue(target,task);
+        int rc = pthread_create(thread,NULL,th_fun,th_arg);
+        if(rc < 0)
+        {
+            printf("Error pthread_create: %d\n",rc);
+            continue;
+        }
+        br.th[i] = thread;
     }
 }
--- src/core/thread_pool/bound_robin/bound_robin.h
+++ src/core/thread_pool/bound_robin/bound_robin.h
@@ -7,14 +7,20 @@
 
 #include "bound_robin.h"
 
-
 typedef struct bound_robin
 {
     int                        current_index;
-    threadsafe_simple_queue_t *event_in[FRIXIA_WORKERS];
     pthread_t                 *th[FRIXIA_WORKERS];
+    void                     (*delegate_function)(void *);
+    void                      *delegate_argument;
 
 } bound_robin_t;
+
+
+bound_robin_t bound_robin_create(void *th_fun(void *),
+                                 void *th_arg,
+                                 void *th_delegate_fun(void *),
+                                 void *th_delegate_arg);
 
 int bound_robin_add_task(bound_robin_t *br, void *task);
 int bound_robin_broadcast_task(bound_robin_t *br, void *task);
