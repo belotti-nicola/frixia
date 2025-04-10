bc233f9ac43759d5d9ef024e91fb92f0d465ac03
nicolab <nicola.belotti@doss.it>
2025-04-10 11:05:14 +0200

    we implement test

--- test/bound_robin/test_bound_robin.c
+++ test/bound_robin/test_bound_robin.c
@@ -1,25 +1,31 @@
 #include "../../src/core/thread_pool/bound_robin/bound_robin.h"
+#include <stdatomic.h> 
 
-void counting_function(void *arg)
-{
-    int *casted_arg = (int *)arg;
-    (*casted_arg)+arg;
-}
 
 void test_main_function(void *arg)
 {
-    
+    atomic_int *value_ptr = (atomic_int *)arg;
+
+    int value = atomic_load(value_ptr);
+    value++;
+    atomic_store(value_ptr,value);
 }
 
 
 int main()
 {
+    atomic_int test_value = 0;
+    atomic_init(&test_value,0);
 
-    int counter=0;
-    
-    bound_robin_t test;
-    bound_robin_create(&test,counting_function,counter,NULL,NULL);
-    bound_robin_wait(&test);
+    bound_robin_t br; 
+    bound_robin_create(&br,NULL,NULL,test_main_function,NULL);
+    bound_robin_add_task(&br,NULL);
+    bound_robin_add_task(&br,NULL);
+
+    if( test_value != 2 )
+    {
+        return 1;
+    }
 
     return 0;
 
