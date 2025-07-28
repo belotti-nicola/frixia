4b1d002b3f5932ab612d60ca7029e39de182e050
belotti-nicola <belotti.nicola@outlook.it>
2025-07-28 23:06:04 +0200

    void starring like no tomorrow

--- main.c
+++ main.c
@@ -15,10 +15,6 @@
 #include <sys/epoll.h>
 #include <inttypes.h>
 
-
-//forward declaration:
-void SETUP_THE_FEPOLL(frixia_epoll_t *fepoll, bool *keep_looping, int fd, void *anything);
-
 typedef struct th_arg
 {
     bool *keep_looping;
@@ -87,8 +83,12 @@ void *adder_tcp(void *cast_me_to_ctx)
         return NULL;
     }
 
-
-    SETUP_THE_FEPOLL(ctx->cb_fepoll,ctx->cb_keep_looping,reply,ctx->anything);
+    sv_callback_t *tmp = (sv_callback_t *)ctx->anything;
+    frixia_epoll_t *fepoll = ctx->cb_fepoll;
+    fepoll->callbacks_data[reply] = *sv_create_callback(
+        tmp->function,
+        tmp->argument
+    );
 
     int fepoll_fd = ctx->cb_fepoll->fd;
     ret_code = insert_event(fepoll_fd,reply);
@@ -97,6 +97,7 @@ void *adder_tcp(void *cast_me_to_ctx)
         printf("Error inserting!\n");
         return NULL;
     }
+    return NULL;
 }
 
 void *logger(void *cast_me_to_ctx)
@@ -141,64 +142,33 @@ void *main_loop(void *th_arg)
         {
             int       fd  = events[i].fd;
             uint32_t mask = events[i].events_maks;
-            sv_callback_t cb = fepoll->callbacks_data[fd];
-            cb_ctx_t *ctx = (cb_ctx_t *)cb.argument;
-            if ( ctx )
+            void *anything = fepoll->callbacks_data[fd].argument;
+            cb_ctx_t ctx = 
             {
-                ctx->cb_fd = fd;
-                ctx->cb_event_mask = mask;
-                ctx->cb_fepoll = fepoll;
-                ctx->cb_keep_looping = keep_looping;
-            }
-            else 
+                .cb_fd = fd,
+                .cb_event_mask = mask,
+                .cb_fepoll = fepoll,
+                .cb_keep_looping = keep_looping,
+                .anything = anything
+            };
+            void *(*f)(void *) = (void *(*)(void *))fepoll->callbacks_data[fd].function;
+            void   *a          = (void *)&ctx;
+            if ( f == NULL )
             {
-                printf("No succ\n");
-                cb_ctx_t tmp = 
-                {
-                    .cb_fd = fd,
-                    .cb_fepoll = fepoll,
-                    .cb_keep_looping = keep_looping,
-                    .cb_event_mask = mask
-
-                };
-                cb.argument = &tmp;
+                printf("Porcoddio\n");
+                continue;
             }
-            sv_do_callback(&cb);
+            f(a);
+            
         }
         printf("*keep looping:%d\n",*keep_looping);
     }
     printf("End\n");
 }
 
-void SETUP_THE_FEPOLL(frixia_epoll_t *fepoll, bool *keep_looping, int fd, void *anything)
-{
-    if ( fd < 0 )
-    {
-        printf("Error:SETUP_THE_FEPOLL fd < 0\n");
-        return;
-    }
-
-    //TODO: THIS IS BAD THING.
-    //PLUS NO DESTRUCTION/FREE.
-    cb_ctx_t *ctx = malloc(sizeof(cb_ctx_t));
-    if ( ctx == NULL )
-    {
-        printf("Error:SETUP_THE_FEPOLL ctx == NULL\n");
-        return NULL;
-    }
-    ctx->cb_fd = fd;
-    ctx->cb_fepoll = fepoll;
-    ctx->cb_keep_looping = keep_looping;
-    sv_callback_t *cb = sv_create_callback(
-        ((sv_callback_t *)anything)->function,
-        ctx
-    );
-
-    fepoll->callbacks_data[fd] = *cb;
-}
-
 void *adder_http_handler(cb_ctx_t *ctx)
 {
+    printf("s\n");
     th_arg_t *arg = (th_arg_t *)ctx;
     bool *keep_looping     = arg->keep_looping;
     frixia_epoll_t *fepoll = arg->fepoll;
@@ -213,7 +183,10 @@ void *adder_http_handler(cb_ctx_t *ctx)
         return NULL;
     }
 
-    SETUP_THE_FEPOLL(ctx->cb_fepoll,ctx->cb_keep_looping,reply,ctx->anything);
+    sv_callback_t *tmp =  ctx->anything;
+    fepoll->callbacks_data[reply].is_valid = true;
+    fepoll->callbacks_data[reply].function = tmp->function;
+    fepoll->callbacks_data[reply].argument = tmp->argument;
 
     int fepoll_fd = ctx->cb_fepoll->fd;
     ret_code = insert_event(fepoll_fd,reply);
@@ -246,7 +219,7 @@ void *http_handler(cb_ctx_t *ctx)
     }
 
     FHTTP_t fhttp = frixia_parse_request(tmp,bytes_read);
-    printf("event: %d, http_callback bytes_read %d(fd:%d,headers:%d, readsize %d)\n", fd, bytes_read, fd, fhttp.num_headers, 256);
+    printf("event: %d, http_callback bytes_read %ld(fd:%d,headers:%d, readsize %d)\n", fd, bytes_read, fd, fhttp.num_headers, 256);
     if ( fhttp.exit_code == false )
     {
         char end_response[] =
@@ -309,11 +282,7 @@ int main(int argc, char *argv[])
         printf("Error insert event %d\n",__LINE__);
         return -1;
     }
-    cb_ctx_t ctx4;
-    ctx4.cb_fd     = 4;
-    ctx4.cb_fepoll = fepoll;
-    ctx4.cb_keep_looping = &keep_looping;
-    fepoll->callbacks_data[4] = *sv_create_callback(new_fepoll_stop,&ctx4);
+    fepoll->callbacks_data[4] = *sv_create_callback(new_fepoll_stop,NULL);
 
     
     int tcp_fd = start_tcp_listening(8081);
@@ -327,11 +296,10 @@ int main(int argc, char *argv[])
         printf("Error insert event %d\n",__LINE__);
         return -1;
     }
-    cb_ctx_t ctx5;
-    ctx5.anything = sv_create_callback(logger,NULL);
-    fepoll->callbacks_data[5] = *sv_create_callback(adder_tcp,&ctx5);
-
-
+    sv_callback_t *svcb = sv_create_callback(logger,NULL);
+    fepoll->callbacks_data[5] = *sv_create_callback(adder_tcp,svcb);
+    
+    /*
     int tcp_fd_2 = start_tcp_listening(8082);
     if (tcp_fd_2 <= 0 )
     {
@@ -346,7 +314,7 @@ int main(int argc, char *argv[])
     cb_ctx_t ctx6;
     ctx6.anything = sv_create_callback(http_handler,NULL);
     fepoll->callbacks_data[6] = *sv_create_callback(adder_http_handler,&ctx6);
-
+    */
     
     pthread_t th;
     th_arg_t data;
