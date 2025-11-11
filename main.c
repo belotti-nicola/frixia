96b78f061ab7baa7f2c4d03e73f04cbe58adb33c
nbelotti <nicola.belotti-ext@urbanandmainlnes.com>
2025-11-11 09:31:05 +0100

    error closing the fd 4

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include "src/core/filedescriptor/fd_monitor/epoll/fepoll.h"
 #include "src/core/filedescriptor/types/tcp/frixia_tcp.h"
-
+#include <sys/eventfd.h>
+#include <sys/epoll.h>
 #include <stdint.h>
 #include <inttypes.h>
 #include <sys/socket.h>
+#include <string.h>
+#include <unistd.h>
+
+
+#define ITERATIONS 5
 
 char HTTP_OK[] = 
     "HTTP/1.1 200 OK\r\n"
@@ -12,14 +18,59 @@ char HTTP_OK[] =
     "\r\n"
     "Greetings from frixia engine";
 
+int utility_event_fd_wake(int efd)
+{
+    int waker_fd = eventfd(0, EFD_NONBLOCK);
+    
+    struct epoll_event ev = { .events = EPOLLIN, .data.fd = waker_fd };
+    epoll_ctl(efd, EPOLL_CTL_ADD, waker_fd, &ev);
+
+    printf("Waker fd:%d\n",waker_fd);
+
+    return waker_fd;
+}
+
+void *waker_th(void *arg)
+{
+    int sleep_time = ITERATIONS;
+    int sleep_duration = 1;
+
+    int *fd = (int *)arg;
+
+    printf("Thread started. Will wake epoll in %d. Countdown about to start\n",sleep_time);
+    while ( sleep_time > 0 )
+    {
+        printf("\t-%d seconds to wake...\n",sleep_time);
+        sleep(1);
+        sleep_time -= 1;
+        printf("About to waking epoll...\n");
+        int *fd = (int *)arg;
+        uint64_t one = 1;
+        int bytes_written = write(*fd, &one, sizeof(one));
+        if ( bytes_written <= 0 )
+        {
+            printf("Error %d!\n",*fd);
+        }
+    }
+
+    
+    
+    return NULL;
+}
+
+
 int main(int argc, char *argv[])
 {      
     frixia_epoll_t *fepoll = create_frixia_epoll();
 
     fepoll_add_tcp_socket_listening(fepoll,"0.0.0.0",10800);
+    int wfd = utility_event_fd_wake(fepoll->fd);
     
-    
-    for(int i=0;i<2;i++)
+
+    pthread_t th;
+    void *arg = (void *)&wfd;
+    pthread_create(&th,NULL,waker_th,arg);
+    for(int i=0;i<ITERATIONS;i++)
     {
         frixia_event_t fevents[10];
         int reply=1;
@@ -30,18 +81,28 @@ int main(int argc, char *argv[])
         }
         for(int j=0;j<fevents_dim;j++)
         {
-            printf("fd %d mask %" PRIu32 "\n",fevents[j].fd,fevents[j].events_maks);
-            int ans = accept_tcp(fevents[j].fd,&reply);
-            if ( ans != 0 )
+            if ( fevents[j].fd == 4 )
             {
-                printf("errno! %d for fd %d\n",ans,fevents[j]);
-                continue;
+
+                printf("fd %d mask %" PRIu32 "\n",fevents[j].fd,fevents[j].events_maks);
+                int ans = accept_tcp(fevents[j].fd,&reply);
+                if ( ans != 0 )
+                {
+                    printf("errno! %d for fd %d\n",ans,fevents[j].fd);
+                    continue;
+                }
+                printf("New fd: %d\n",reply);
+                
+                int bytes_wrote = write_tcp(reply,HTTP_OK,strlen(HTTP_OK));
+                close_tcp(reply);
+                printf("wrote %d bytes on fd %d (strlen %ld)\n",bytes_wrote,reply,strlen(HTTP_OK));
             }
-            printf("New fd: %d\n",reply);
-            
-            int bytes_wrote = write_tcp(reply,HTTP_OK,strlen(HTTP_OK));
-            close_tcp(reply);
-            printf("wrote %d bytes on fd %d (strlen %d)\n",bytes_wrote,reply,strlen(HTTP_OK));
+            else 
+            {
+                uint64_t val;
+                ssize_t n = read(fevents[j].fd, &val, sizeof(val));
+                printf("%ld (%ld bytes read)\n",val,n);
+            }
         }
 
     }
