89360c68bafcef0666da737329c565170b7d8b1e
Nicola Belotti <belotti.nicola@outlook.it>
2025-11-15 17:52:11 +0100

    better writing in test cases

--- test/frixia_mappings/finodes/test_finodes.c
+++ test/frixia_mappings/finodes/test_finodes.c
@@ -3,18 +3,31 @@
 #include <stdint.h>
 #include <stdio.h>
 
+typedef struct test_data
+{
+    uint64_t test_case;
+    uint64_t test_value;
+
+} test_data_t;
+
 int main()
 {
-    FRIXIA_INODE_FLAG_T m;
+    test_data_t tests[] = 
+    { 
+        {
+            .test_case  = FINODE_ACCESS | FINODE_MODIFY,
+            .test_value = IN_ACCESS     | IN_MODIFY
+        },
+    };
 
-    m = FINODE_ACCESS | FINODE_MODIFY;
-    int8_t check = m;
-    int8_t value = IN_ACCESS | IN_MODIFY;
-    if( check != value )
+    size_t dim = sizeof(tests)/sizeof(test_data_t);
+    for (size_t i = 0; i < dim; i++)
     {
-        printf("Check failed: %d %d",check,value);
-        return 1;
+        if (tests[i].test_case != tests[i].test_value) {
+            printf("Check number %d failed: %lu != %lu\n",i, tests[i].test_case, tests[i].test_value);
+            return 1;
+        }
     }
-
+    
     return 0;
 }
--- test/frixia_mappings/fsignals/test_fsignals.c
+++ test/frixia_mappings/fsignals/test_fsignals.c
@@ -3,17 +3,29 @@
 #include <stdint.h>
 #include <stdio.h>
 
+typedef struct test_data
+{
+    uint64_t test_case;
+    uint64_t test_value;
+
+} test_data_t;
+
 int main()
 {
-    FRIXIA_SIGNALS_T m;
+    test_data_t tests[] = { 
+        {
+            .test_case  = FSIGNAL_INT | FSIGNAL_HUP,
+            .test_value = SIGINT      | SIGHUP
+        },
+    };
 
-    m = FSIGNAL_INT | FSIGNAL_HUP;
-    int8_t check = m;
-    int8_t value = SIGINT | SIGHUP;
-    if( check != value )
+    size_t dim = sizeof(tests)/sizeof(test_data_t);
+    for (size_t i = 0; i < dim; i++)
     {
-        printf("Check failed: %d %d",check,value);
-        return 1;
+        if (tests[i].test_case != tests[i].test_value) {
+            printf("Check number %d failed: %lu != %lu\n",i, tests[i].test_case, tests[i].test_value);
+            return 1;
+        }
     }
 
     return 0;
