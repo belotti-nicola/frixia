50ab3a9c600460a38e827e2ef2d26a4bef649f54
Nicola Belotti <belotti.nicola@outlook.it>
2026-03-11 20:00:55 +0100

    testing signal needs rework

---
+++ test/filedescriptors/signal/test_signal.c
@@ -0,0 +1,67 @@
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+#include <frixia/frixia.h>
+
+void *TEST_CALLBACK(FRIXIA_CALLBACK_CTX *ctx)
+{
+    frixia_environment_t *fenv = ctx->fenv;
+    frixia_stop(fenv);
+    return NULL;
+}
+
+int test()
+{
+    FRIXIA_RESULT res;
+    frixia_environment_t *fenv = frixia_environment_create(10);
+    
+    res = frixia_add_signal(fenv,FSIGNAL_TERM);
+    if( !frixia_result_is_ok(res) )
+    {
+        perror("Error adding signal");
+        return -1;
+    }
+    int fd = frixia_result_fd(res);
+    frixia_register_cb(fenv,fd,TEST_CALLBACK,NULL);
+
+    frixia_start(fenv);
+    frixia_environment_destroy(fenv);
+    return 0;
+}
+
+int main() {
+    setbuf(stderr, NULL);
+    setbuf(stdout, NULL);
+
+
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        return test();
+    }
+
+    sleep(2);
+    kill(pid, SIGTERM);
+
+    int status;
+    waitpid(pid, &status, 0);
+
+    if (WIFSIGNALED(status)) {
+        printf("Process killed by signal: %d\n", WTERMSIG(status));
+        return 1;
+    }
+
+    if (!WIFEXITED(status))
+    {
+        printf("Error WIFEXITED");
+        return 1;
+    }
+    if (WEXITSTATUS(status) != 0)
+    {
+        printf("Error WEXITSTATUS");
+        return 1;
+    }
+
+    return 0;
+}
